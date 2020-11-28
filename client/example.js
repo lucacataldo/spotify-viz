e42ef9ff423c22b0ecb3d69bcd0bfb88e5511ae3
Luca Cataldo <35984346+lucacataldo@users.noreply.github.com>
2020-11-28 17:27:02 -0500

    mod

--- client/example.js
+++ client/example.js
@@ -1,35 +1,49 @@
 import Visualizer from './classes/visualizer'
 import { interpolateRgb, interpolateBasis } from 'd3-interpolate'
 import { getRandomElement } from './util/array'
-import { sin, circle } from './util/canvas'
+import { sin, circle, polygon } from './util/canvas'
 
 export default class Example extends Visualizer {
-  constructor () {
+  constructor() {
     super({ volumeSmoothing: 10 })
-    this.theme = ['#18FF2A', '#7718FF', '#06C5FE', '#FF4242', '#18FF2A']
+    this.theme = ['#F7567C', '#F5B82E', '#5E4AE3']
   }
 
-  hooks () {
+  hooks() {
     this.sync.on('bar', beat => {
       this.lastColor = this.nextColor || getRandomElement(this.theme)
       this.nextColor = getRandomElement(this.theme.filter(color => color !== this.nextColor))
     })
   }
 
-  paint ({ ctx, height, width, now }) {
+  paint({ ctx, height, width, now }) {
     const bar = interpolateBasis([0, this.sync.volume * 10, 0])(this.sync.bar.progress)
     const beat = interpolateBasis([0, this.sync.volume * 300, 0])(this.sync.beat.progress)
-    ctx.fillStyle = 'rgba(0, 0, 0, .08)'
+    
+    let brightness = beat /100 ;
+    ctx.fillStyle = `hsla(73, 0%, ${5-brightness}%, 0.5)`
     ctx.fillRect(0, 0, width, height)
-    ctx.lineWidth = bar 
-    ctx.strokeStyle = interpolateRgb(this.lastColor, this.nextColor)(this.sync.bar.progress)
-    sin(ctx, now / 50, height / 2, this.sync.volume * 50, 100)
+
+    ctx.strokeStyle = "#ffffff";
+
+    ctx.beginPath();
+    const points = polygon(3+(6 * (beat/100) ), 100 + (bar * 10), width / 2, height / 2, this.sync.beat.progress*270)
+    points.forEach((e, i) => {
+      ctx.lineTo(e.x, e.y)
+    })
+    ctx.closePath()
     ctx.stroke()
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)'
-    ctx.beginPath()
-    ctx.lineWidth = beat
-    circle(ctx, width / 2, height / 2, this.sync.volume * height / 5 + beat / 10)
-    ctx.stroke()
-    ctx.fill()
+
+    if (beat > 100) {
+      ctx.beginPath()
+      polygon(3, 50, width/2, height/2, 90).forEach(e=>{
+        ctx.lineTo(e.x, e.y)
+      })
+      ctx.closePath()
+      ctx.fillStyle = "#7D6167"
+      ctx.fill()
+    }
+
+    
   }
 }
--- client/index.js
+++ client/index.js
@@ -3,8 +3,7 @@ import Template from './template'
 import Example from './example'
 
 if (window.location.hash === '#start') {
-  const template = new Template()
-  // const example = new Example()
+  const example = new Example()
 } else {
   auth()
 }
